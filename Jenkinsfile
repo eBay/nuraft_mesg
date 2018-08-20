c9817b471cdc985aedcf198743d80cd328d6d221
Brian Szmyd <bszmyd@ebay.com>
2018-08-20 10:29:48 -0600

    Update raft_core to 2018.08.20

--- Jenkinsfile
+++ Jenkinsfile
@@ -41,7 +41,7 @@ pipeline {
 
     post {
         always {
-            sh "docker rmi ${PROJECT}"
+            sh "docker rmi -f ${PROJECT}"
         }
     }
 }
--- conanfile.py
+++ conanfile.py
@@ -5,27 +5,43 @@ from conans import ConanFile, CMake, tools
 class RaftCoreGRPCConan(ConanFile):
     name = "raft_core_grpc"
     version = "0.3.2"
+
     license = "Apache 2.0"
     url = "https://github.corp.ebay.com/SDS/raft_core_grpc"
     description = "A gRPC service for raft_core"
 
-    settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = "shared=False", "fPIC=True"
+    settings = "arch", "os", "compiler", "build_type"
+    options = {"shared": ['True', 'False'],
+               "fPIC": ['True', 'False'],
+               "coverage": ['True', 'False']}
+    default_options = 'shared=False', 'fPIC=True', 'coverage=False'
 
     requires = (("grpc/1.14.1@oss/stable"),
-                ("raft_core/0.1.1@oss/stable"))
+                ("raft_core/2018.08.20@oss/stable"))
 
     generators = "cmake"
-
     exports = ["LICENSE.md"]
     exports_sources = "CMakeLists.txt", "cmake/*", "src/*"
 
+    def configure(self):
+        if not self.settings.compiler == "gcc":
+            del self.options.coverage
+
+
     def build(self):
         cmake = CMake(self)
-        cmake.configure()
+
+        definitions = {'CONAN_BUILD_COVERAGE': 'OFF',
+                       'CMAKE_EXPORT_COMPILE_COMMANDS': 'ON'}
+        test_target = None
+
+        if self.options.coverage == 'True':
+            definitions['CONAN_BUILD_COVERAGE'] = 'ON'
+            test_target = 'coverage'
+
+        cmake.configure(defs=definitions)
         cmake.build()
-        cmake.test()
+        cmake.test(target=test_target)
 
     def package(self):
         self.copy("*.lib", dst="lib", keep_path=False)
@@ -39,3 +55,5 @@ class RaftCoreGRPCConan(ConanFile):
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
+        if self.options.coverage == 'True':
+            self.cpp_info.libs.append('gcov')
