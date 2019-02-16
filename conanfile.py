afe0751a78a484870784058910f1a190c7b99b71
Brian Szmyd <bszmyd@ebay.com>
2019-02-16 13:19:21 +0000

    Upgrade gRPC

--- conanfile.py
+++ conanfile.py
@@ -25,7 +25,7 @@ class RaftCoreGRPCConan(ConanFile):
     requires = (
             "lzma/5.2.4@bincrafters/stable",
             "raft_core/2019.01.24@oss/testing",
-            "sds_grpc/1.0.3@sds/testing",
+            "sds_grpc/1.1.1@sds/testing",
             "sds_logging/3.6.0@sds/testing"
         )
 
