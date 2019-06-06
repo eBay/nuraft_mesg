263ee01e4d3d184e18316e26f207694743fc22af
Brian Szmyd <bszmyd@ebay.com>
2019-06-06 09:34:38 -0700

    v0.10.10

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ from conans import ConanFile, CMake, tools
 
 class NuPillarGRPCConan(ConanFile):
     name = "nupillar_grpc"
-    version = "0.10.9"
+    version = "0.10.10"
 
     license = "Apache 2.0"
     url = "https://github.corp.ebay.com/SDS/nupillar_grpc"
@@ -24,9 +24,9 @@ class NuPillarGRPCConan(ConanFile):
 
     requires = (
             "lzma/5.2.4@bincrafters/stable",
-            "nupillar/2019.05.28@oss/testing",
+            "nupillar/2019.06.06@oss/testing",
             "sds_grpc/1.1.4@sds/testing",
-            "sds_logging/5.0.0@sds/testing"
+            "sds_logging/5.1.0@sds/testing"
         )
 
     generators = "cmake"
