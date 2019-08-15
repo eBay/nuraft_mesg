bfd35e4e26c44238c8bde71c242882f2c829611f
Brian Szmyd <bszmyd@ebay.com>
2019-08-15 10:27:13 -0700

    Update logging

--- conanfile.py
+++ conanfile.py
@@ -22,9 +22,9 @@ class NuRaftGRPCConan(ConanFile):
 
     requires = (
             "lzma/5.2.4@bincrafters/stable",
-            "nuraft/2019.08.09@oss/develop",
+            "nuraft/2019.08.12@oss/develop",
             "sds_grpc/1.1.7@sds/testing",
-            "sds_logging/5.2.0@sds/testing"
+            "sds_logging/5.3.2@sds/develop"
         )
 
     generators = "cmake"
