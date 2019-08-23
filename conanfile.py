7d516bbf3f8943ebe92281d540c33d74d403fdb9
Brian Szmyd <bszmyd@ebay.com>
2019-08-23 12:33:58 -0700

    v0.11.10 - Upgrade OpenSSL and NuRaft.

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ from conans import ConanFile, CMake, tools
 
 class NuRaftGRPCConan(ConanFile):
     name = "nuraft_grpc"
-    version = "0.11.9"
+    version = "0.11.10"
 
     license = "Apache 2.0"
     url = "https://github.corp.ebay.com/SDS/nuraft_grpc"
@@ -22,8 +22,8 @@ class NuRaftGRPCConan(ConanFile):
 
     requires = (
             "lzma/5.2.4@bincrafters/stable",
-            "nuraft/2019.08.12@oss/develop",
-            "sds_grpc/1.1.7@sds/testing",
+            "nuraft/2019.08.23@oss/develop",
+            "sds_grpc/1.1.8@sds/develop",
             "sds_logging/5.3.2@sds/develop"
         )
 
