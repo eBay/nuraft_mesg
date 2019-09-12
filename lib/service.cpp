fa403990407e0cfbd356a97523bea04f0d6ff25c
Brian Szmyd <bszmyd@ebay.com>
2019-09-12 14:34:58 -0700

    Start v0.13.13

--- lib/service.cpp
+++ lib/service.cpp
@@ -20,47 +20,39 @@ using AsyncRaftSvc = Messaging::AsyncService;
 
 msg_service::~msg_service() = default;
 
-void
-msg_service::associate(::sds::grpc::GrpcServer* server) {
-        assert(server);
-        if (!server->register_async_service<Messaging>()) {
-            LOGERRORMOD(nuraft, "Could not register RaftSvc with gRPC!");
-            abort();
-        }
+void msg_service::associate(::sds::grpc::GrpcServer* server) {
+    RELEASE_ASSERT(server, "NULL server!");
+    if (!server->register_async_service< Messaging >()) {
+        LOGERRORMOD(nuraft, "Could not register RaftSvc with gRPC!");
+        abort();
+    }
 }
 
-void
-msg_service::bind(::sds::grpc::GrpcServer* server) {
-        assert(server);
-        if (!server->register_rpc<Messaging, RaftGroupMsg, RaftGroupMsg>(
+void msg_service::bind(::sds::grpc::GrpcServer* server) {
+    RELEASE_ASSERT(server, "NULL server!");
+    if (!server->register_rpc< Messaging, RaftGroupMsg, RaftGroupMsg >(
             &AsyncRaftSvc::RequestRaftStep,
             std::bind(&msg_service::raftStep, this, std::placeholders::_1, std::placeholders::_2))) {
-            LOGERRORMOD(nuraft, "Could not bind gRPC ::RaftStep to routine!");
-            abort();
-        }
+        LOGERRORMOD(nuraft, "Could not bind gRPC ::RaftStep to routine!");
+        abort();
+    }
 }
 
-::grpc::Status
-msg_service::raftStep(RaftGroupMsg& request, RaftGroupMsg& response) {
-    auto const &group_name = request.group_name();
+::grpc::Status msg_service::raftStep(RaftGroupMsg& request, RaftGroupMsg& response) {
+    auto const& group_name = request.group_name();
 
     auto const& base = request.message().base();
     LOGTRACEMOD(sds_msg, "Received [{}] from: [{}] to: [{}] Group: [{}]",
-            nuraft::msg_type_to_string(nuraft::msg_type(base.type())),
-            base.src(),
-            base.dest(),
-            group_name
-            );
+                nuraft::msg_type_to_string(nuraft::msg_type(base.type())), base.src(), base.dest(), group_name);
 
     if (nuraft::join_cluster_request == base.type()) {
         joinRaftGroup(base.dest(), group_name);
     }
 
-    shared<sds::grpc_server> server;
+    shared< sds::grpc_server > server;
     {
-        std::shared_lock<lock_type> rl(_raft_servers_lock);
-        if (auto it = _raft_servers.find(group_name);
-                _raft_servers.end() != it) {
+        std::shared_lock< lock_type > rl(_raft_servers_lock);
+        if (auto it = _raft_servers.find(group_name); _raft_servers.end() != it) {
             COUNTER_INCREMENT(*it->second.m_metrics, group_steps, 1);
             server = it->second.m_server;
         }
@@ -70,9 +62,7 @@ msg_service::raftStep(RaftGroupMsg& request, RaftGroupMsg& response) {
     if (server) {
         try {
             return server->step(*request.mutable_message(), *response.mutable_message());
-        } catch (std::runtime_error& rte) {
-            LOGERRORMOD(sds_msg, "Caught exception during step(): {}", rte.what());
-        }
+        } catch (std::runtime_error& rte) { LOGERRORMOD(sds_msg, "Caught exception during step(): {}", rte.what()); }
     } else {
         LOGWARNMOD(sds_msg, "Missing RAFT group: {}", group_name);
     }
@@ -83,40 +73,46 @@ msg_service::raftStep(RaftGroupMsg& request, RaftGroupMsg& response) {
 // do not need to bind the grpc_servers to anything...just piggy-backing
 // on their ::step() and transformations.
 class null_service final : public sds::grpc_server {
- public:
+public:
     using sds::grpc_server::grpc_server;
-    void associate(sds::grpc::GrpcServer*) override {};
-    void bind(sds::grpc::GrpcServer*) override {};
+    void associate(sds::grpc::GrpcServer*) override{};
+    void bind(sds::grpc::GrpcServer*) override{};
 };
 
-void
-msg_service::joinRaftGroup(int32_t const srv_id, group_name_t const& group_name) {
+void msg_service::joinRaftGroup(int32_t const srv_id, group_name_t const& group_name) {
     LOGINFOMOD(sds_msg, "Joining RAFT group: {}", group_name);
 
-    std::unique_lock<lock_type> lck(_raft_servers_lock);
-    auto [it, happened] = _raft_servers.emplace(std::make_pair(group_name, group_name));
-    if (_raft_servers.end() == it || !happened) return;
-
-    nuraft::context* ctx {nullptr};
-    if (auto err = _get_server_ctx(srv_id, group_name, ctx, it->second.m_metrics, this); err) {
-        LOGERRORMOD(sds_msg,
-                    "Error during RAFT server creation on group {}: {}",
-                    group_name,
-                    err.message());
-        return;
+    nuraft::context* ctx{nullptr};
+    bool             happened{false};
+    auto             it = _raft_servers.end();
+    {
+        std::unique_lock< lock_type > lck(_raft_servers_lock);
+        std::tie(it, happened) = _raft_servers.emplace(std::make_pair(group_name, group_name));
+        if (_raft_servers.end() != it) {
+            RELEASE_ASSERT(happened, "Could not insert new RAFT group.");
+            if (happened) {
+                if (auto err = _get_server_ctx(srv_id, group_name, ctx, it->second.m_metrics, this); err) {
+                    LOGERRORMOD(sds_msg, "Error during RAFT server creation on group {}: {}", group_name,
+                                err.message());
+                    return;
+                }
+                RELEASE_ASSERT(ctx, "Could not retrieve RAFT context.");
+            }
+        }
     }
-    assert(ctx != nullptr);
+    if (_raft_servers.end() == it || !happened)
+        return;
 
-    auto server = std::make_shared<nuraft::raft_server>(ctx);
-    it->second.m_server = std::make_shared<null_service>(server);
+    auto server = std::make_shared< nuraft::raft_server >(ctx);
+    it->second.m_server = std::make_shared< null_service >(server);
 }
 
-void
-msg_service::partRaftGroup(group_name_t const& group_name) {
+void msg_service::partRaftGroup(group_name_t const& group_name) {
     LOGINFOMOD(sds_msg, "Parting RAFT group: {}", group_name);
-    shared<nuraft::raft_server> server;
+    shared< nuraft::raft_server > server;
 
-    {   std::unique_lock<lock_type> lck(_raft_servers_lock);
+    {
+        std::unique_lock< lock_type > lck(_raft_servers_lock);
         if (auto it = _raft_servers.find(group_name); _raft_servers.end() != it) {
             server = it->second.m_server->raft_server();
             _raft_servers.erase(it);
@@ -128,4 +124,4 @@ msg_service::partRaftGroup(group_name_t const& group_name) {
 
     server->shutdown();
 }
-}
+} // namespace sds::messaging
