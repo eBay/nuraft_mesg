c59a49cb0125af31085cb816d0b7b5e8e2889f9c
Brian Szmyd <bszmyd@ebay.com>
2020-03-16 14:15:45 -0600

    v0.14.3 : Update Jungle and NuRAFT.

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ from conans import ConanFile, CMake, tools
 
 class NuRaftGRPCConan(ConanFile):
     name = "nuraft_grpc"
-    version = "0.14.2"
+    version = "0.14.3"
 
     license = "Apache 2.0"
     url = "https://github.corp.ebay.com/SDS/nuraft_grpc"
@@ -22,7 +22,7 @@ class NuRaftGRPCConan(ConanFile):
         )
 
     requires = (
-            "nuraft/1.4.7@sds/develop",
+            "nuraft/1.4.9@sds/develop",
             "sds_grpc/1.1.11@sds/master",
             "sds_logging/7.0.0@sds/develop"
         )
