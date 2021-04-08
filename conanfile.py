c63e7ca5768a81193dcda592a180e60c002a34c5
Brian Szmyd <bszmyd@ebay.com>
2021-04-08 14:31:56 +0000

    Upgrade gRPC.

--- conanfile.py
+++ conanfile.py
@@ -22,11 +22,9 @@ class NuRaftGRPCConan(ConanFile):
         )
 
     requires = (
-            "nuraft/[~=1.6, include_prerelease=True]@nudata/master",
-            "sds_grpc/[~=1, include_prerelease=True]@sds/master",
+            "nuraft/[~=1.8, include_prerelease=True]@nudata/master",
+            "sds_grpc/[~=2, include_prerelease=True]@sds/master",
             "sds_logging/[~=9, include_prerelease=True]@sds/master",
-
-            "openssl/1.1.1g",
         )
 
     generators = "cmake"
