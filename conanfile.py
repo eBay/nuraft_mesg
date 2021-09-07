41bb9fb83afd489025e03d41f0746d916f3f3328
Kadayam, Harihara(hkadayam) <hkadayam@ebay.com>
2021-09-07 14:39:58 -0700

    Fixing dependent pkg version

--- conanfile.py
+++ conanfile.py
@@ -16,7 +16,7 @@ class NuRaftGRPCConan(ConanFile):
     generators = "cmake"
     requires = (
                 "nuraft/[~=1.8, include_prerelease=True]@nudata/master",
-                "grpc_helper/[~=1, include_prerelease=True]@sisl/develop",
+                "grpc_helper/[~=1, include_prerelease=True]@sisl/master",
                 "sds_logging/[~=10, include_prerelease=True]@sds/master",
                 )
     options = {
