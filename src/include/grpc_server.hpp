537608ac9365e7b1d4f221c6c5239dc85ae7ebd0
raakella <raakella@ebay.com>
2022-04-18 12:55:19 -0700

    Add get server config api in raft server

--- src/include/grpc_server.hpp
+++ src/include/grpc_server.hpp
@@ -37,6 +37,8 @@ public:
     bool request_leadership();
     void yield_leadership(bool immediate = false);
 
+    void get_srv_config_all(std::vector< nuraft::ptr< nuraft::srv_config > >& configs_out);
+
     nuraft::ptr< nuraft::cmd_result< nuraft::ptr< nuraft::buffer > > >
     append_entries(const std::vector< nuraft::ptr< nuraft::buffer > >& logs);
 
