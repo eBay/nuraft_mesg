cb747a33eb70e902153d4c0c90bbc4da1fd92392
Brian Szmyd <bszmyd@ebay.com>
2022-11-23 10:48:41 -0700

    Update grpc_helper to 4.x

--- conanfile.py
+++ conanfile.py
@@ -41,7 +41,7 @@ class NuRaftGRPCConan(ConanFile):
 
     def requirements(self):
         self.requires("nuraft/nbi.20221102")
-        self.requires("grpc_helper/[~=3, include_prerelease=True]@sisl/master")
+        self.requires("grpc_helper/[~=4, include_prerelease=True]@sisl/master")
         self.requires("openssl/1.1.1s")
         self.requires("sisl/[~=8, include_prerelease=True]@oss/master")
 
