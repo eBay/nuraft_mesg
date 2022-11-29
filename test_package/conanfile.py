0d57d56a8db9e125ca5e4bbf78c7a6f1e52a7bb1
Brian Szmyd <bszmyd@ebay.com>
2022-11-29 11:34:49 -0700

    Update lz4

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -11,7 +11,7 @@ class TestPackageConan(ConanFile):
     def requirements(self):
         self.requires("nlohmann_json/3.10.5")
         self.requires("jungle_logstore/nbi.20221129@sds/master")
-        self.requires("lz4/1.9.3", override=True)
+        self.requires("lz4/1.9.4", override=True)
 
     def build(self):
         cmake = CMake(self)
