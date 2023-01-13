2a49719aecc61ea1e7f85df93e5d248d6435a9cc
Brian Szmyd <bszmyd@ebay.com>
2023-01-13 13:24:36 -0700

    Reset version to alpha stage. Changes to come.

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conans import ConanFile, CMake, tools
 
 class NuRaftMesgConan(ConanFile):
     name = "nuraft_mesg"
-    version = "1.0.0"
+    version = "0.0.1"
     homepage = "https://github.com/eBay/nuraft_mesg"
     description = "A gRPC service for nuraft"
     topics = ("ebay", "nublox", "raft")
