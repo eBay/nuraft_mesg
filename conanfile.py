70e015bcd0c903464780d44c26efd12b3f0d8fe3
Xiaoxi Chen <xiaoxchen@ebay.com>
2024-01-24 02:22:04 -0700

    Version to 2.3.1
    Signed-off-by: Xiaoxi Chen <xiaoxchen@ebay.com>

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,7 @@ required_conan_version = ">=1.50.0"
 
 class NuRaftMesgConan(ConanFile):
     name = "nuraft_mesg"
-    version = "2.3.2"
+    version = "2.3.1"
 
     homepage = "https://github.com/eBay/nuraft_mesg"
     description = "A gRPC service for NuRAFT"
