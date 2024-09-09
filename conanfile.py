9b50b3fbb44b12817303b9d32ee4efdd13ad0a8e
Ravi Nagarjun Akella <raakella1@$HOSTNAME>
2024-09-08 17:09:12 -0700

    shutdown raft services before grpc server

--- conanfile.py
+++ conanfile.py
@@ -10,7 +10,7 @@ required_conan_version = ">=1.60.0"
 
 class NuRaftMesgConan(ConanFile):
     name = "nuraft_mesg"
-    version = "3.5.10"
+    version = "3.5.11"
 
     homepage = "https://github.com/eBay/nuraft_mesg"
     description = "A gRPC service for NuRAFT"
