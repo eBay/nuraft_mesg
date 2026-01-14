aea99658e2d921e05f4848d603aa82c81eaad8bb
yawzhang <yawzhang@ebay.com>
2026-01-14 20:44:51 +0800

    update nuraft version to 2.4.9
    This change add nuraft new fix on add_member and add more logs on snapshot handler

--- prepare_v2.sh
+++ prepare_v2.sh
@@ -6,5 +6,5 @@ conan export 3rd_party/forestdb --name forestdb --version cci.20250315 >/dev/nul
 echo -n "jungle."
 conan export 3rd_party/jungle --name jungle --version cci.20250316 >/dev/null
 echo -n "nuraft."
-conan export 3rd_party/nuraft --name nuraft --version 2.4.8 >/dev/null
+conan export 3rd_party/nuraft --name nuraft --version 2.4.9 >/dev/null
 echo "done."
